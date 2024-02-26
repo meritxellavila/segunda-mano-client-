8111f38b30ff36113c018a40d5cf3f291d08d7a1
Adri Priego <priegolucena@gmail.com>
2024-02-26 10:52:42 +0100

    Eliminar subcarpeta

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+
+function HomePage() {
+    const [allProducts, setAllProducts] = useState(null)
+
+    useEffect(() => {
+        axios.get
+    })
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HomePage
