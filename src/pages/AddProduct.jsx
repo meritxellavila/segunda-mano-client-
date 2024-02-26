bb51443eb7b0495f69e7fcdd5e9cdce6eaa24534
meritxellavila <meriavila66@gmail.com>
2024-02-26 17:36:49 +0100

    style

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useState } from "react";
-import axios from 'axios';
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import axios from "axios";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
-import InputGroup from 'react-bootstrap/InputGroup';
+import InputGroup from "react-bootstrap/InputGroup";
+import { Form, Button, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
 
 function AddProduct() {
+  const navigate = useNavigate();
+
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
   const [description, setDescription] = useState("");
@@ -14,7 +17,7 @@ function AddProduct() {
   const [checkboxes, setCheckboxes] = useState({
     nuevo: false,
     usado: false,
-  })
+  });
   const [keyWords, setKeyWords] = useState("");
   const [category, setCategory] = useState("");
 
@@ -40,13 +43,12 @@ function AddProduct() {
     setImg(inputImg);
   };
 
-
- const handleCheckboxChange = (checkboxName) => {
-     setCheckboxes((preventCheckBoxes) => ({
-         ...preventCheckBoxes,
-         [checkboxName]: !preventCheckBoxes[checkboxName],
-     }));
- }
+  const handleCheckboxChange = (checkboxName) => {
+    setCheckboxes((preventCheckBoxes) => ({
+      ...preventCheckBoxes,
+      [checkboxName]: !preventCheckBoxes[checkboxName],
+    }));
+  };
 
   const handleKeyWords = (event) => {
     let inputValue = event.target.value;
@@ -67,13 +69,17 @@ function AddProduct() {
         price: price,
         description: description,
         image: img,
-        nuevo: checkboxes.nuevo,  
-        usado: checkboxes.usado, 
+        nuevo: checkboxes.nuevo,
+        usado: checkboxes.usado,
         keyWords: keyWords,
         category: category,
       };
-      const response = await axios.post(`http://localhost:5005/products`, newProduct);
+      const response = await axios.post(
+        `http://localhost:5005/products`,
+        newProduct
+      );
       console.log(response);
+     
 
       setName("");
       setPrice(0);
@@ -85,103 +91,98 @@ function AddProduct() {
       });
       setKeyWords("");
       setCategory("");
-      
+      navigate("/")
     } catch (error) {
       console.log(error);
     }
   };
 
-
-
   return (
-    <div>
-      <h1>Añadir nuevo Producto</h1>
+    <>
+      <Container>
+        <h1>Añadir nuevo Producto</h1>
 
-      <Form onSubmit={handleSubmit}>
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Nombre:"
-          className="mb-3"
-        >
-          <Form.Control type="text" value={name} onChange={handleName} />
-        </FloatingLabel>
+        <Form onSubmit={handleSubmit}>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Nombre:"
+            className="mb-3"
+          >
+            <Form.Control type="text" value={name} onChange={handleName} />
+          </FloatingLabel>
 
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Price:"
-          className="mb-3"
-        >
-          <Form.Control type="number" value={price} onChange={handlePrice} />
-        </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Price:"
+            className="mb-3"
+          >
+            <Form.Control type="number" value={price} onChange={handlePrice} />
+          </FloatingLabel>
 
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Description:"
-          className="mb-3"
-        >
-          <Form.Control
-            type="text"
-            value={description}
-            onChange={handleDescription}
-          />
-        </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Description:"
+            className="mb-3"
+          >
+            <Form.Control
+              type="text"
+              value={description}
+              onChange={handleDescription}
+            />
+          </FloatingLabel>
 
-        <FloatingLabel
-          controlId="floatingInput"
-          label="URL Img:"
-          className="mb-3"
-        >
-          <Form.Control type="text" value={img} onChange={handleImg} />
-        </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="URL Img:"
+            className="mb-3"
+          >
+            <Form.Control type="text" value={img} onChange={handleImg} />
+          </FloatingLabel>
 
-        <FloatingLabel
-          controlId="floatingInput"
-          label="KeyWords:"
-          className="mb-3"
-        >
-          <Form.Control type="text" value={keyWords} onChange={handleKeyWords} />
-        </FloatingLabel>
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Category:"
-          className="mb-3"
-        >
-          <Form.Control
-            type="text"
-            value={category}
-            onChange={handleCategory}
-          />
-        </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="KeyWords:"
+            className="mb-3"
+          >
+            <Form.Control
+              type="text"
+              value={keyWords}
+              onChange={handleKeyWords}
+            />
+          </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Category:"
+            className="mb-3"
+          >
+            <Form.Control
+              type="text"
+              value={category}
+              onChange={handleCategory}
+            />
+          </FloatingLabel>
 
-
-        <InputGroup className="mb-3 container"> 
-        <Form.Check
-            type="checkbox"
-            label="Nuevo"
-            checked={checkboxes.nuevo}
-            onChange={() => handleCheckboxChange('nuevo')}
+          <InputGroup className="mb-3 container">
+            <Form.Check
+              type="checkbox"
+              label="Nuevo"
+              checked={checkboxes.nuevo}
+              onChange={() => handleCheckboxChange("nuevo")}
             />
             <Form.Check
-            type="checkbox"
-            label="Usado"
-            checked={checkboxes.usado}
-            onChange={() => handleCheckboxChange('usado')}
-
+              type="checkbox"
+              label="Usado"
+              checked={checkboxes.usado}
+              onChange={() => handleCheckboxChange("usado")}
             />
-        </InputGroup>
-        
-        
-   
+          </InputGroup>
 
-       
-
-        <div>
           <Button variant="outline-secondary" size="lg" type="submit">
             Añadir
           </Button>
-        </div>
-      </Form>
-    </div>
+        </Form>
+      </Container>
+    </>
   );
 }
 
