1a76904127ebd560789cded31367b07b002c93f1
Adri Priego <priegolucena@gmail.com>
2024-02-27 10:51:19 +0100

    antes de favoritos

--- src/pages/Favoritos.jsx
+++ src/pages/Favoritos.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Favoritos() {
+function Favoritos(props) {
+
+  const [favProduct, setFavProduct] = useState([])
+
+
   return (
     <div>
-      
+      <h2>Tus Favoritos:</h2>
+
+      {favProduct.map((producto, index) => {
+        return (
+          <h2>{}</h2>
+        )
+      })}
+
     </div>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,9 +3,11 @@ import axios from "axios";
 import API_URL from "../utils/api"
 import Navbar from '../components/Navbar';
 import { Link } from 'react-router-dom';
+import ListProducts from "./ListProducts";
 
 function HomePage() {
     const [allProducts, setAllProducts] = useState(null)
+
 
     useEffect(() => {
         axios.get(`${API_URL}/products`)
@@ -30,18 +32,28 @@ function HomePage() {
                 {allProducts.map((eachProduct) => {
                     return (
                         <div key={eachProduct.id}>
-                            <Link to={`/ProductDetall/${eachProduct.id}`}>
-                                <img src={eachProduct.img} alt="imagen"  width={"100%"}/>
-                                <h2>{eachProduct.name}</h2>
-                                <h2>{eachProduct.description}</h2>
-                                <p>{eachProduct.price}</p>
+                            <Link to={`${API_URL}/ProductDetall/${eachProduct.id}`}>
+                                <img id='imagen-productos' src={eachProduct.img} alt="imagen" />
+                                <div className='propiedades'>
+                                    <h2>{eachProduct.name}</h2>
+                                    <h4>{eachProduct.description}</h4>
+                                    <p>{eachProduct.price}€</p>
+                                </div>
                             </Link>
                         </div>
                     )
                 })}
 
+
+            {/*<Route
+                path="/list-products"
+                render={() => <ListProducts allProducts={allProducts} />}
+            />*/}
+
         </div>
     )
+    
+
 }
 
 export default HomePage
